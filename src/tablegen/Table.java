ffb7c23ecf8f9a843b28f63f3df05715ba9d8429
ktrrahn <ktrrahn@gmail.com>
2020-11-11 15:51:30 -0500

    Update Table.java
    add more attributes to Table class

--- src/tablegen/Table.java
+++ src/tablegen/Table.java
@@ -3,6 +3,10 @@ package tablegen;
 import java.util.ArrayList;
 
 public class Table {
+private double tableTopLength;
+private double tableTopWidth;
+private double tableOverhang;
+
 Leg[] tableLegs = new Leg[4];
 ArrayList<TableComponent> tableComponents = new ArrayList<>();
 Slat[] tableSlats = new Slat[3];
@@ -10,4 +14,30 @@ Slat[] tableSlats = new Slat[3];
 public double calculatePrice() {
 return 0.0;	
 }
+
+public double getTableTopLength() {
+	return tableTopLength;
+}
+
+public void setTableTopLength(double tableTopLength) {
+	this.tableTopLength = tableTopLength;
+}
+
+public double getTableTopWidth() {
+	return tableTopWidth;
+}
+
+public void setTableTopWidth(double tableTopWidth) {
+	this.tableTopWidth = tableTopWidth;
+}
+
+public double getTableOverhang() {
+	return tableOverhang;
+}
+
+public void setTableOverhang(double tableOverhang) {
+	this.tableOverhang = tableOverhang;
+}
+
+
 }
