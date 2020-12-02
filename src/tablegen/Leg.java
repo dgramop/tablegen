d4c5496ddd87659231466c3b1c9cb92ffff6d726
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2020-12-02 17:43:09 -0500

    fix pricing

--- src/tablegen/Leg.java
+++ src/tablegen/Leg.java
@@ -52,31 +52,4 @@ public double calculateCubeFoot() {
 	return height*width*length;
 }
 
-public double getPrice(ApplianceStore store) {
-	double price = 0.0;
-	switch(store) {
-	case HomeDepot:
-		if(height<=8/2) {
-			price = 4.98/2.0;
-		}
-		else {
-			price = 4.98;
-		}
-		//https://www.homedepot.com/p/2-in-x-4-in-x-92-625-in-Premium-Kiln-Dried-Whitewood-Stud-569062/302778078
-		break;
-	case Ikea:
-		break;
-	case Lowes:
-		if(height<=10/2) {
-			price = 6.62/2.0;
-		}
-		else {
-			price = 6.62;
-		}
-		//https://www.lowes.com/pd/2-in-x-4-in-x-10-ft-Whitewood-Lumber-Common-2-in-x-4-in-x-10-ft-Actual/1001134500
-		break;
-	}
-	return price;
-}
-
 }
