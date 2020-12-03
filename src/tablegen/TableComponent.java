fb4ae7be78e33bde5bf55e93ff9ed1e9266cc525
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2020-12-02 22:49:42 -0500

    remove dead code in comments

--- src/tablegen/TableComponent.java
+++ src/tablegen/TableComponent.java
@@ -1,8 +1,6 @@
 package tablegen;
 
 public interface TableComponent {
-//public abstract void setX(double x);
-//public abstract void setY(double y);
 public abstract double getPrice(ApplianceStore store);
 
 }
