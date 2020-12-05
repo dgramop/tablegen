db26369bf7de40854b5798c9618d1b60318b5a76
ktrrahn <ktrrahn@gmail.com>
2020-12-04 21:15:53 -0500

    price
    class not found

--- src/tablegen/Prices.java
+++ src/tablegen/Prices.java
@@ -1,11 +1,20 @@
 package tablegen;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+
 
 public class Prices {
 	
@@ -30,6 +39,80 @@ public class Prices {
 // 	--compressed
 
 // {"data":{"product":{"itemId":"100070209","dataSources":"catalog","info":{"recommendationFlags":{"visualNavigation":false,"__typename":"RecommendationFlags"},"replacementOMSID":null,"hasSubscription":false,"minimumOrderQuantity":0,"projectCalculatorEligible":false,"productDepartment":"21","classNumber":"5","subClassNumber":"19","calculatorType":null,"isLiveGoodsProduct":false,"protectionPlanSku":null,"hasServiceAddOns":false,"consultationType":null,"__typename":"Info"},"availabilityType":{"discontinued":false,"type":"Store Only","status":false,"buyable":true,"__typename":"AvailabilityType"},"fulfillment":{"backordered":false,"fulfillmentOptions":[{"type":"pickup","fulfillable":true,"services":[{"type":"bopis","locations":[{"isAnchor":true,"inventory":{"isLimitedQuantity":false,"isOutOfStock":false,"isInStock":true,"quantity":27,"isUnavailable":false,"maxAllowedBopisQty":null,"minAllowedBopisQty":null,"__typename":"Inventory"},"type":"store","storeName":"Fairfax","locationId":"4601","curbsidePickupFlag":true,"isBuyInStoreCheckNearBy":null,"distance":null,"state":"VA","storePhone":"(703)266-9800","__typename":"Location"}],"deliveryTimeline":null,"deliveryDates":null,"deliveryCharge":null,"dynamicEta":null,"hasFreeShipping":false,"freeDeliveryThreshold":null,"totalCharge":null,"__typename":"Service"}],"__typename":"FulfillmentOption"},{"type":"delivery","fulfillable":true,"services":[{"type":"express delivery","locations":[{"isAnchor":false,"inventory":null,"type":"store","storeName":null,"locationId":"4601","curbsidePickupFlag":null,"isBuyInStoreCheckNearBy":null,"distance":null,"state":null,"storePhone":null,"__typename":"Location"}],"deliveryTimeline":"TOMORROW","deliveryDates":{"startDate":"2020-12-05","endDate":"2020-12-05","__typename":"DeliveryDate"},"deliveryCharge":"79.0","dynamicEta":{"hours":null,"minutes":null,"__typename":"DynamicEta"},"hasFreeShipping":false,"freeDeliveryThreshold":null,"totalCharge":79,"__typename":"Service"}],"__typename":"FulfillmentOption"}],"anchorStoreStatus":true,"anchorStoreStatusType":"ACTIVE","backorderedShipDate":null,"bossExcludedShipStates":"AK,GU,HI,PR,VI","excludedShipStates":"AK,GU,HI,PR,VI","seasonStatusEligible":null,"onlineStoreStatus":false,"onlineStoreStatusType":"DELETED","inStoreAssemblyEligible":false,"__typename":"Fulfillment"},"seoDescription":"Melamine White Panel is ideal for making cabinet carcass, desk tops, closets, store fixtures and furniture. Easy to maintain and clean.","identifiers":{"brandName":"Veranda","productLabel":"Melamine White Panel (Common: 3/4 in. x 4 ft. x 8 ft.; Actual: .750 in. x 49 in. x 97 in.)","productType":"MERCHANDISE","storeSkuNumber":"461877","isSuperSku":false,"parentId":null,"sampleId":null,"modelNumber":"461877","__typename":"Identifiers"},"media":{"images":[{"url":"https://images.homedepot-static.com/productImages/604278a9-44ba-40b7-b3b0-ed0d15b2cac1/svn/veranda-mdf-461877-64_<SIZE>.jpg","sizes":["65","100","145","300","400","600","1000"],"type":"IMAGE","subType":"PRIMARY","__typename":"Image"}],"__typename":"Media"},"pricing":{"value":28.97,"alternatePriceDisplay":false,"unitOfMeasure":"each","alternate":{"bulk":null,"unit":{"caseUnitOfMeasure":null,"unitsPerCase":null,"value":null,"__typename":"UnitPricing"},"__typename":"Alternate"},"original":null,"promotion":null,"__typename":"Pricing"},"reviews":{"ratingsReviews":{"averageRating":"4.346","totalReviews":"315","__typename":"RatingsReviews"},"__typename":"Reviews"},"badges":[],"installServices":{"scheduleAMeasure":false,"__typename":"InstallServices"},"specificationGroup":[{"specTitle":"Details","specifications":[{"specName":"Engineered Wood Type","specValue":"Particle Board/MDF","__typename":"Specification"},{"specName":"Span Rating","specValue":"Not Applicable","__typename":"Specification"},{"specName":"Weather Exposure","specValue":"Interior","__typename":"Specification"},{"specName":"Plywood Features","specValue":"None","__typename":"Specification"}],"__typename":"SpecificationGroup"},{"specTitle":"Warranty / Certifications","specifications":[{"specName":"Manufacturer Warranty","specValue":"See store for details.","__typename":"Specification"}],"__typename":"SpecificationGroup"},{"specTitle":"Dimensions","specifications":[{"specName":"Nominal Product Thickness (in.)","specValue":"3/4","__typename":"Specification"},{"specName":"Actual Product Length (in.)","specValue":"97.","__typename":"Specification"},{"specName":"Actual Product Thickness (in.)","specValue":".75","__typename":"Specification"},{"specName":"Actual Product Width (in.)","specValue":"49","__typename":"Specification"},{"specName":"Nominal Product Length (ft.)","specValue":"8","__typename":"Specification"},{"specName":"Nominal Product Width (ft.)","specValue":"4","__typename":"Specification"}],"__typename":"SpecificationGroup"}],"subscription":null,"__typename":"BaseProduct"}}}
+	
+	
+	public static String executePost(String targetURL, String urlParameters) {
+		  HttpURLConnection connection = null;
+
+		  try {
+		    //Create connection
+		    URL url = new URL(targetURL);
+		    connection = (HttpURLConnection) url.openConnection();
+		    connection.setRequestMethod("POST");
+		    connection.setRequestProperty("authority", 
+		        "www.homedepot.com");
+		    connection.setRequestProperty("accept", 
+			        "*/*");
+		    connection.setRequestProperty("x-debug", 
+			        "false");
+		    connection.setRequestProperty("dnt", 
+			        "1");
+		    connection.setRequestProperty("x-experience-name", 
+			        "general-merchandise");
+		    connection.setRequestProperty("x-api-cookies", 
+			        "{\"x-user-id\":\"dbb69e11-75fb-f0d1-a940-9926445ce257\"}");
+		    connection.setRequestProperty("user-agent", 
+			        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36");
+		    connection.setRequestProperty("content-type", 
+			        "application/json");
+		    connection.setRequestProperty("origin", 
+			        "https://www.homedepot.com");
+		    connection.setRequestProperty("sec-fetch-site", 
+			        "same-origin");
+		    connection.setRequestProperty("sec-fetch-mode", 
+			        "cors");
+		    connection.setRequestProperty("sec-fetch-dest", 
+			        "empty");
+		    connection.setRequestProperty("referer", 
+			        "https://www.homedepot.com/p/Veranda-Melamine-White-Panel-Common-3-4-in-x-4-ft-x-8-ft-Actual-750-in-x-49-in-x-97-in-461877/100070209");
+		    connection.setRequestProperty("accept-language", 
+			        "en-US,en;q=0.9,es;q=0.8");
+		    connection.setRequestProperty("cookie", 
+			        "THD_CACHE_NAV_PERSIST=; thda.u=dbb69e11-75fb-f0d1-a940-9926445ce257; RES_TRACKINGID=54618987594440800; _abck=F543955C66F37ECFFF29B61FDDEDE54A~0~YAAQnpcwFxNLr1V0AQAAdm1htwRB4O475kWEjN9/zoaWxA/JIDA0AGCBg7yMeJaeoLeV+fJQmM7wQC+gaau65W+yhs3CEYsdYIQQOvV/QZjhxd7Y8elMM+dlGWznMdWpuUXaFPxRuRN8xLaK87V6ZJ2U4b/s5YSSWO3erF50OjRurDTLbaCn7y0VI6gRkNFuKM/A//tfzJT4eVZhrneWycMT2JQ/x147jzvtTouE0Ds1yqszgN1poj658wkc++BfAAunLdScoUE6dnFjxtH3uQ4C0VMtvYSHnncq1x7iOacsd6rBBx1NbdjEoQy0nBJ6E1iSiwcWwRWIUQ==~-1~-1~-1; _pxvid=90e7fa61-102b-11eb-bb68-0242ac12000c; _px_f394gi7Fvmc43dfg_user_id=OTEzNDE1YzAtMTAyYi0xMWViLTllMzctNGJkYmQ2NGNkZTM2; ftr_ncd=6; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%227face3ee-67e4-49df-88ed-ec14fd2a4bf9%22; _meta_bing_beaconFired=true; _meta_facebookPixel_beaconFired=true; _meta_merkle_securedVisitBeaconFired=true; _meta_merkle_rkdmsBeaconFired=true; _gcl_au=1.1.1148877102.1602906491; aam_mobile=seg%3D1131078; aam_uuid=25866415413445447774516779719598253283; LPVID=hiNTVjNThjMzhjM2VjNTI3; DELIVERY_ZIP_TYPE=AUTO; _meta_adobe_aam_uuid=25866415413445447774516779719598253283; _meta_metarouter_timezone_offset=300; QSI_SI_2lVW226zFt4dVJ3_intercept=true; _meta_mediaMath_iframe_counter=5; HD_DC=origin; check=true; AMCVS_F6421253512D2C100A490D45%40AdobeOrg=1; THD_FORCE_LOC=1; WORKFLOW=LOC_HISTORY_BY_IP; THD_INTERNAL=0; DELIVERY_ZIP=22030; thda.s=6bb6a9f5-1b1a-c08a-5e52-dc3f0af2ae4a; _meta_movableInk_mi_u=7face3ee-67e4-49df-88ed-ec14fd2a4bf9; thda.m=25588232375374905564524305436100528790; ResonanceSegment=1; THD_GUESTLIST=NwsKZgTE3jsZ6VHxi4AfWAa4oEh6B61+7wCHU6Xzhl3j1jpHSs7ubktOhY7LTL1fcY2CynpXaddF8r/GtGHSteq5PWw0q3LvQlE639gVXa4=; THD_SESSION=; THD_CACHE_NAV_SESSION=; SEARCH_HST=; QSI_HistorySession=https%3A%2F%2Fwww.homedepot.com%2Fp%2FVeranda-Melamine-White-Panel-Common-3-4-in-x-4-ft-x-8-ft-Actual-750-in-x-49-in-x-97-in-461877%2F100070209~1607037493184; bm_sz=4F080D0000C95A52EFF597ADB871A9B4~YAAQj5cwF3xopS12AQAAcVyZLgoLfVrdcY8ttTsrjVdeIDZBOMILR3ICiP60hp/HMvQ0LqIg2zH13okvVnPSs/MnK24HPZRMlU1u6IfawjYNOAXwJ+686/CEBwkH61S/RxOP2fwotb4jrPhOCzDpkR4w2sRYAJXAwLYK1/rZp+7iNkX2Fm5JBuzPKaXkRYnPKg+E; THD_NR=1; mbox=session#0cf173e2d24944e8ac8073efe390fa71#1607101432; THD_PERSIST=C4%3D4601%2BFairfax%20-%20Fairfax%2C%20VA%2B%3A%3BC4_EXP%3D1638400490%3A%3BC24%3D22030%3A%3BC24_EXP%3D1638400490%3A%3BC39%3D1%3B8%3A00-20%3A00%3B2%3B6%3A00-21%3A00%3B3%3B6%3A00-21%3A00%3B4%3B6%3A00-21%3A00%3B5%3B6%3A00-21%3A00%3B6%3B6%3A00-21%3A00%3B7%3B6%3A00-21%3A00%3A%3BC39_EXP%3D1607103176; THD_LOCALIZER=%7B%22WORKFLOW%22%3A%22LOC_HISTORY_BY_IP%22%2C%22THD_FORCE_LOC%22%3A%221%22%2C%22THD_INTERNAL%22%3A%220%22%2C%22THD_STRFINDERZIP%22%3A%2222030%22%2C%22THD_LOCSTORE%22%3A%224601%2BFairfax%20-%20Fairfax%2C%20VA%2B%22%2C%22THD_STORE_HOURS%22%3A%221%3B8%3A00-20%3A00%3B2%3B6%3A00-21%3A00%3B3%3B6%3A00-21%3A00%3B4%3B6%3A00-21%3A00%3B5%3B6%3A00-21%3A00%3B6%3B6%3A00-21%3A00%3B7%3B6%3A00-21%3A00%22%2C%22THD_STORE_HOURS_EXPIRY%22%3A1607103176%7D; ak_bmsc=FDE58CD0627454318BB6C7DC6F8CD3A61730978FF2210000B364CA5F2639FC30~plDCDjvby8rfRdA5EdkaeLNqHUKyF8X1qFDDGOgmRcv3rhxr99ImNxvTfX8G8g3tcD5QCjHFT6YUia4KnsBdgke0K7FEBsC8MZqiH17ZlXhy8TVOoIdBPXJJI2Z0lGDktglzyGrmcfZvi7zetlZpgsKQVgSYwde0wxVOc7Iix2a1sxW8rxwcxNmFLw1BwFaRk9cIqMAIXg1/OQ8iuzIJ+lnSSGCdfPOFRO/X7eYHXV7DL//5IqeMFj+j3dIXjYNFAQ; RES_SESSIONID=88890480788599070; AMCV_F6421253512D2C100A490D45%40AdobeOrg=1585540135%7CMCIDTS%7C18600%7CMCMID%7C25588232375374905564524305436100528790%7CMCAAMLH-1607704392%7C7%7CMCAAMB-1607704392%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1607106793s%7CNONE%7CMCCIDH%7C-1826263068%7CvVersion%7C4.4.0; LPSID-31564604=f7F3S2ZvSZWHsyVrg5NnEQ; IN_STORE_API_SESSION=TRUE; _pxff_cc=U2FtZVNpdGU9TGF4Ow==; _px=L505fATUKHSCXsTc2iNm9h2V9dOzpqI6ot+B8/y+OzEiTnCRUnV6SI/4AYVTfKI3/PXciiyTgHBJZWNDqindEA==:1000:sC0IyUmpqbbNXm4RRXsofN6zSigeyZpUQFU808jijzclEmhQat0/exeESkCQBQ8L/UzUKQWvmwB3g2GxxDNffVYrZMU/K/sXvnya70Dnk+CTQ3ACNWcHAPkIPITwCJpRb5fCSQgKFfZHqi6sXuSJtqCRIdR0JFmlDsXeROryknRBvpqZAH20umti6ylsLeJO7GyjEBXAay5AN9ZsVBnJv6vbOJILjvW1jM5iDybpNH/0dMXNxAsRoipgQAi9Cw0cHKu3JOBQ1OZYRtKMWv/VTg==; s_pers=%20productnum%3D7%7C1609457624832%3B%20s_nr365%3D1607100416650-Repeat%7C1638636416650%3B%20s_dslv%3D1607100416660%7C1701708416660%3B; s_sq=%5B%5BB%5D%5D; s_sess=%20stsh%3D%3B%20s_pv_pName%3Dproductdetails%253E100070209%3B%20s_pv_pType%3Dpip%3B%20s_pv_cmpgn%3D%3B%20s_pv_pVer%3D%3B%20s_cc%3Dtrue%3B; forterToken=84d5c11b77b442b4a48e83bf392e6bd8_1607100416721__UDF43_9ck; _pxde=69640180095360ab463b3d781a5bfe63a45dfd868454d44156828fb143c36fb4:eyJ0aW1lc3RhbXAiOjE2MDcxMDA0MzMyNTF9; _px_4946459675_cs=eyJpZCI6IjYyYjM1MTQwLTM2NGUtMTFlYi04YWZjLTczODgyODJhYmI4ZSIsInN0b3JhZ2UiOnt9LCJleHBpcmF0aW9uIjoxNjA3MTAyMjQzNDkxfQ==; akaau=1607100743~id=63463ce29e71859204f6ec3e0110c4c7; bm_sv=86EC464F67948CDB6190E2527EA627FB~baV42RiBZudpflhFfE/2bPZZrojLYDbIhvuO6zPqD+uspQL/yu72ouetcgcxeCWMDiJGvmTtjE+sV+5mbgJ6J62kOU4TdLgT8Wx+9VZekQOynoPPL6bb6K+23e+893GW+5dv/nVhnQFRojF6nAznVTbBMUgQ1bxmIERMRibRHVQ=");
+		    connection.setRequestProperty("sec-gpc", 
+			        "1");
+		    
+
+		    connection.setUseCaches(false);
+		    connection.setDoOutput(true);
+
+		    //Send request
+		    DataOutputStream wr = new DataOutputStream (
+		        connection.getOutputStream());
+		    wr.writeBytes(urlParameters);
+		    wr.close();
+
+		    //Get Response  
+		    InputStream is = connection.getInputStream();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		      response.append(line);
+		      response.append('\r');
+		    }
+		    rd.close();
+		    return response.toString();
+		  } catch (Exception e) {
+		    e.printStackTrace();
+		    return null;
+		  } finally {
+		    if (connection != null) {
+		      connection.disconnect();
+		    }
+		  }
+		}
+	
 	
 	private static Document getDocument(ApplianceStore store,Object item) throws IOException
 	{
@@ -66,14 +149,18 @@ public class Prices {
 		}
 		return 0.0d;
 	}
-	
-	public static void main(String args[])
+
+	public static void main(String[] args)
 	{
-		try {
+		System.out.println("Hi");
+		//System.out.println(executePost("https://www.homedepot.com/product-information/model", null));      
+		    
+		
+		/*try {
 			getPrice(ApplianceStore.HomeDepot);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 }
